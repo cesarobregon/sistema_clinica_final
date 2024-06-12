7d4c55b855652914478d1f23f8a57ab6581633e3
cesarobregon <cesarobregon66@gmail.com>
2024-06-12 00:54:10 -0300

    empezando a documentar

--- src/entidades/Especialidad.java
+++ src/entidades/Especialidad.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Clase para manejar la informacion de Especialidad
  */
 package entidades;
 
@@ -9,22 +8,26 @@ package entidades;
  *
  * @author OBREGON
  */
+
 public class Especialidad {
+    //atributos de la clase
     private int id;
     private String descripcion;
     
+    //metodo constructor vacio, para inicializar un objeto de tipo Especialidad
     public Especialidad(){
         this.setId(0);
         this.setDescripcion("");
     }
     
+    //metodo constructor para instanciar un objeto de tipo Especialidad
     public Especialidad(int id, String descripcion){
         this.setId(id);
         this.setDescripcion(descripcion);
     }
     
+    //metodos get y set, para obtener o cambiar los valores de los atributos del objeto
     
-
     public int getId() {
         return id;
     }
@@ -41,10 +44,8 @@ public class Especialidad {
         this.descripcion = descripcion;
     }
 
-    
-
+    //
     public Object[] toObject() {
-
         Object[] info = new Object[]{getId(),getDescripcion()};
         return info;
     }
--- src/entidades/Profesional.java
+++ src/entidades/Profesional.java
@@ -1,3 +1,6 @@
+/*
+ * Clase para manejar la informacion de Profesional
+ */
 package entidades;
 /**
  *
